a862b57a4f6998e75e6940f9648993cd36d0f326
노지용(업무용) <5240njy@gmail.com>
2026-01-07 16:04:30 +0900

    Update data.js

--- data.js
+++ data.js
@@ -211,7 +211,7 @@ const teamYearData = [
 // ==================== 설정 옵션 ====================
 const config = {
     // 회사/팀 이름
-    teamName: '우리 팀22',
+    teamName: '우리 팀',
     
     // 연도
     year: '2025',
